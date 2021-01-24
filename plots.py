2ded2131ca5ad57e50a9bb76c72c9a488a7103fc
Narges Alavi Samani <narges.alavi73@gmail.com>
2021-01-23 20:39:57 -0500

    Changes in plot.

--- plots.py
+++ plots.py
@@ -25,7 +25,7 @@ plt.scatter(x, y, marker='o',color='blue',label='L=2,w=4,d=1')
 y = np.array([0.116092,0.103657,0.312526,np.nan,np.nan,np.nan,np.nan])
 plt.scatter(x, y, marker='s',color='purple',label='L=4,w=1,d=1')
 # for l=4,w=2,d=1
-y = np.array([np.nan,np.nan,0.375075,0.325434,np.nan,np.nan,np.nan])
+y = np.array([np.nan,np.nan,0.375075,0.325434,np.nan,0.398591,0.660803])
 plt.scatter(x, y, marker='s',color='green',label='L=4,w=2,d=1')
 # for l=4,w=3,d=1
 # for l=4,w=4,d=1
@@ -49,7 +49,7 @@ plt.scatter(x, y,marker='s',facecolors='none',edgecolors='green',label='L=4,w=2,
 
 plt.grid(b=True, which='both', color='#666666', linestyle='--')
 
-plt.legend()
+plt.legend(bbox_to_anchor=(1.001, 1), loc='upper left')
 plt.xlabel("Circuit ID", fontsize=13)
 plt.ylabel("Cost Function After 300 Steps", fontsize=13)
 plt.show()
